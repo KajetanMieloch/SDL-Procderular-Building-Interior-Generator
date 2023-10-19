c68aaf3e3c27d284ffaa9f2715bce831a336391c
KajetanMieloch <kajetanmieloch@gmail.com>
2023-10-19 19:46:24 +0200

    Add game.hpp and game.cpp
    Chcnged file structure. Now it is more clear

--- main.cpp
+++ main.cpp
@@ -1,60 +1,23 @@
-#include <iostream>
-#include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include "game.hpp"
+#include "game.cpp"
 
-int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
-        return 1;
+
+Game *game = NULL;
+
+int main(int argc, const char* argv[]) {
+
+    game = new Game();
+    
+    game->init("Interior generator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
     }
 
-    SDL_Window* window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (TTF_Init() < 0) {
-        std::cerr << "SDL_ttf initialization failed: " << TTF_GetError() << std::endl;
-        return 1;
-    }
-
-    TTF_Font* font = TTF_OpenFont("res/Arial.ttf", 24);
-
-    if (!font) {
-        std::cerr << "Failed to load font: " << TTF_GetError() << std::endl;
-        return 1;
-    }
-
-    SDL_Color textColor = {255, 0, 0, 255};
-    SDL_Surface* textSurface = TTF_RenderText_Solid(font, "Hello, World!", textColor);
-    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-
-    SDL_Event event;
-    bool quit = false;
-
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);  // Set background color to black
-        SDL_RenderClear(renderer);
-
-        // Draw the "Hello, World!" text
-        SDL_RenderCopy(renderer, textTexture, NULL, NULL);
-
-        // Update the screen
-        SDL_RenderPresent(renderer);
-    }
-
-    SDL_DestroyTexture(textTexture);
-    SDL_FreeSurface(textSurface);
-    TTF_CloseFont(font);
-    TTF_Quit();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    game->clean();
 
     return 0;
 }
